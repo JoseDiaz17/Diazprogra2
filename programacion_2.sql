a7e474c0557682511f41b63c16d534b226d18815
JoseDiaz17 <50640072+JoseDiaz17@users.noreply.github.com>
2020-03-16 23:37:45 -0600

    bd

--- programacion_2.sql
+++ programacion_2.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
+-- version 4.8.0.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 12-03-2020 a las 00:06:38
--- Versión del servidor: 5.7.15-log
--- Versión de PHP: 5.6.26
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-03-2020 a las 06:37:12
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,6 +21,21 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `programacion_2`
 --
+CREATE DATABASE IF NOT EXISTS `programacion_2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `programacion_2`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+CREATE TABLE `bitacora` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `accion` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -34,6 +51,34 @@ CREATE TABLE `clientes` (
   `telefono` int(11) NOT NULL,
   `email` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `descuentos`
+--
+
+CREATE TABLE `descuentos` (
+  `id_des` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `descuento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empresas`
+--
+
+CREATE TABLE `empresas` (
+  `id_empresa` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `id_horario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -54,7 +99,7 @@ CREATE TABLE `estado` (
 
 CREATE TABLE `horarios` (
   `id_horario` int(11) NOT NULL,
-  `id_cliente` int(11) NOT NULL,
+  `id_empresa` int(11) NOT NULL,
   `fecha_inicio` int(11) NOT NULL,
   `fecha_fin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
@@ -62,16 +107,63 @@ CREATE TABLE `horarios` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `reserva`
+-- Estructura de tabla para la tabla `ofertas`
 --
 
-CREATE TABLE `reserva` (
+CREATE TABLE `ofertas` (
+  `id_oferta` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `id_empresa` int(11) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `descripcion` varchar(256) NOT NULL,
+  `estado_oferta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `referidos`
+--
+
+CREATE TABLE `referidos` (
+  `id_referidos` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefono` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservaciones`
+--
+
+CREATE TABLE `reservaciones` (
   `id_reserva` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
-  `id_cita` int(11) NOT NULL,
+  `id_servicio` int(11) NOT NULL,
   `fecha_inico` date NOT NULL,
-  `fecha_fin` date NOT NULL
+  `fecha_fin` date NOT NULL,
+  `hora` time NOT NULL,
+  `id_estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicios`
+--
+
+CREATE TABLE `servicios` (
+  `id_servicio` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `id_empresa` int(11) NOT NULL,
+  `precio` int(11) NOT NULL,
+  `descripcion` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,10 +184,33 @@ CREATE TABLE `usuarios` (
 --
 
 --
+-- Indices de la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_usuario` (`id_usuario`);
+
+--
 -- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `descuentos`
+--
+ALTER TABLE `descuentos`
+  ADD PRIMARY KEY (`id_des`),
+  ADD UNIQUE KEY `id_servicio` (`id_servicio`);
+
+--
+-- Indices de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  ADD PRIMARY KEY (`id_empresa`),
+  ADD UNIQUE KEY `id_servicio` (`id_servicio`),
+  ADD UNIQUE KEY `id_servicio_2` (`id_servicio`),
+  ADD UNIQUE KEY `id_horario` (`id_horario`);
 
 --
 -- Indices de la tabla `estado`
@@ -108,16 +223,45 @@ ALTER TABLE `estado`
 --
 ALTER TABLE `horarios`
   ADD PRIMARY KEY (`id_horario`),
+  ADD UNIQUE KEY `id_cliente` (`id_empresa`),
+  ADD UNIQUE KEY `id_cliente_2` (`id_empresa`),
+  ADD UNIQUE KEY `id_empresa` (`id_empresa`);
+
+--
+-- Indices de la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD PRIMARY KEY (`id_oferta`),
+  ADD UNIQUE KEY `id_empresa` (`id_servicio`),
+  ADD UNIQUE KEY `id_servicio` (`id_empresa`);
+
+--
+-- Indices de la tabla `referidos`
+--
+ALTER TABLE `referidos`
+  ADD PRIMARY KEY (`id_referidos`),
+  ADD UNIQUE KEY `id_usuario` (`id_cliente`),
+  ADD UNIQUE KEY `id_servicio` (`id_servicio`),
   ADD UNIQUE KEY `id_cliente` (`id_cliente`);
 
 --
--- Indices de la tabla `reserva`
+-- Indices de la tabla `reservaciones`
 --
-ALTER TABLE `reserva`
+ALTER TABLE `reservaciones`
   ADD PRIMARY KEY (`id_reserva`),
-  ADD UNIQUE KEY `id_cita` (`id_cita`),
+  ADD UNIQUE KEY `id_cita` (`id_servicio`),
+  ADD UNIQUE KEY `id_servicio` (`id_servicio`),
+  ADD UNIQUE KEY `id_estado` (`id_estado`),
+  ADD UNIQUE KEY `id_cliente_2` (`id_cliente`),
   ADD KEY `id_cliente` (`id_cliente`),
-  ADD KEY `id_cita_2` (`id_cita`);
+  ADD KEY `id_cita_2` (`id_servicio`);
+
+--
+-- Indices de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD UNIQUE KEY `id_empresa` (`id_empresa`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -130,39 +274,120 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `bitacora`
+--
+ALTER TABLE `bitacora`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
   MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `descuentos`
+--
+ALTER TABLE `descuentos`
+  MODIFY `id_des` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  MODIFY `id_empresa` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `estado`
 --
 ALTER TABLE `estado`
   MODIFY `id_estado` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `horarios`
 --
 ALTER TABLE `horarios`
   MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT de la tabla `reserva`
+-- AUTO_INCREMENT de la tabla `ofertas`
 --
-ALTER TABLE `reserva`
+ALTER TABLE `ofertas`
+  MODIFY `id_oferta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `referidos`
+--
+ALTER TABLE `referidos`
+  MODIFY `id_referidos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reservaciones`
+--
+ALTER TABLE `reservaciones`
   MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `reserva`
+-- Filtros para la tabla `bitacora`
 --
-ALTER TABLE `reserva`
-  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `horarios` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `bitacora`
+  ADD CONSTRAINT `bitacora_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
+
+--
+-- Filtros para la tabla `descuentos`
+--
+ALTER TABLE `descuentos`
+  ADD CONSTRAINT `descuentos_ibfk_1` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`);
+
+--
+-- Filtros para la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  ADD CONSTRAINT `empresas_ibfk_1` FOREIGN KEY (`id_horario`) REFERENCES `horarios` (`id_horario`);
+
+--
+-- Filtros para la tabla `ofertas`
+--
+ALTER TABLE `ofertas`
+  ADD CONSTRAINT `ofertas_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id_empresa`),
+  ADD CONSTRAINT `ofertas_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`);
+
+--
+-- Filtros para la tabla `referidos`
+--
+ALTER TABLE `referidos`
+  ADD CONSTRAINT `referidos_ibfk_3` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`);
+
+--
+-- Filtros para la tabla `reservaciones`
+--
+ALTER TABLE `reservaciones`
+  ADD CONSTRAINT `reservaciones_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`),
+  ADD CONSTRAINT `reservaciones_ibfk_3` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`),
+  ADD CONSTRAINT `reservaciones_ibfk_4` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id_cliente`);
+
+--
+-- Filtros para la tabla `servicios`
+--
+ALTER TABLE `servicios`
+  ADD CONSTRAINT `servicios_ibfk_1` FOREIGN KEY (`id_empresa`) REFERENCES `empresas` (`id_empresa`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
